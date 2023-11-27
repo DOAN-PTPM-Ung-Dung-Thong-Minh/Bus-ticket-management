bdb360023cfebead838b02ff4689034a3f28c265
Hồ Thanh Tùng <105987148+Vantangvantang@users.noreply.github.com>
2023-11-27 22:22:43 +0700

    Làm Giao Diện  Website(trang chủ,liên hệ, giới thiệu, trang khách hàng)

---
+++ UngDung/WebSite_QL_VeXe/WebSite_QL_VeXe/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite_QL_VeXe.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: TrangChu
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+
+
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+
+        //=========================Register/Login/Logout======================
+
+
+    }
+}
---
+++ UngDung/WebSite_QL_VeXe/WebSite_QL_VeXe/Controllers/KhachHangController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite_QL_VeXe.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        // GET: KhachHang
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+
+
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        public ActionResult LienHe()
+        {
+            return View();
+        }
+    }
+}
